8b78f98afda2b792780fed1ba19cfc6b7ef0c695
Blaise deB Frederick <blaise.frederick@gmail.com>
2022-08-30 15:43:45 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ import rapidtide
 pdf_break_level = 2
 
 # generate autosummary even if no references
-autosummary_generate = True
+autosummary_generate = False
 autodoc_default_flags = ["members", "inherited-members"]
 add_module_names = True
 
